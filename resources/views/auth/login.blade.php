6cc995f88c717016acea57ad030291b39ee0a27e
Gabriel Hoverman <GabeHoverman@Gabriels-MacBook-Pro-507.local>
2017-10-25 12:36:41 -0400

    Added link back to home from login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -100,7 +100,7 @@
                     <div class="columns">
                         <div class="column is-8 is-offset-2">
                             <h1 class=" has-text-centered section">
-                                <img src="../assets/img/HouseMateLogoDark.png">
+                                <a href="/"><img src="../assets/img/HouseMateLogoDark.png"></a>
                             </h1>
                             <!-- AUTHO Login Form -->
                             <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
